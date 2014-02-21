3817a235aada54052b11a76d9c27527384df8f96
simonkazakov <simonkazakov@gmail.com>
2014-02-21 22:40:57 +0200

    Initial implementation for player and camera movement.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[AddComponentMenu("Transform/Follow Transform")]
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 10.0f;
+    public float height = 5.0f;
+    public float heightDamping = 2.0f;
+
+    private void LateUpdate()
+    {
+        if (!target)
+        {
+            return;
+        }
+
+        var wantedHeight = target.position.y + height;
+        var currentHeight = transform.position.y;
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        transform.position = target.position;
+
+        // Set the height of the camera
+        transform.position = new Vector3(target.position.x, currentHeight, target.position.z);
+        transform.position -= Vector3.forward * distance;
+
+        // Always look at the target
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float Speed = 20f;
+    private CharacterController controller;
+    private Vector3 targetPosition;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+    
+
+    // Use this for initialization
+    private void Start()
+    {
+        targetPosition = transform.position;
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            targetPosition = GetTargetPosition(Input.mousePosition);    
+        }
+        MoveToPosition(targetPosition);
+
+    }
+
+    private void MoveToPosition(Vector3 position)
+    {
+        if (Vector3.Distance(transform.position, position) >= 1)
+        {
+            Quaternion rotation = Quaternion.LookRotation(position - transform.position);
+            rotation.x = 0f;
+            rotation.z = 0f;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10);
+            controller.SimpleMove(transform.forward * Speed);
+            animation.CrossFade(run.name);
+        }
+        else
+        {
+            animation.CrossFade(idle.name);
+        }
+    }
+
+    private Vector3 GetTargetPosition(Vector3 positionAtScreen)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(positionAtScreen);
+        RaycastHit hit;
+
+        Vector3 hitPosition = targetPosition;
+
+        if (Physics.Raycast(ray, out hit))
+        {            
+            if (hit.collider.tag != "Player")
+            {
+                Debug.Log("hi");
+                hitPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            }
+        }
+
+        return hitPosition;
+    }
+}
