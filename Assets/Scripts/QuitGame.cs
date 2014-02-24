e3e408442d0fd52a08c1b2c3a954b55480628e86
gornakoff <nikolaygornakov@yahoo.com>
2014-02-25 01:00:30 +0200

    Adding Main menu scene and scripts.

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class QuitGame : MonoBehaviour
+{
+    // change the text color when button pointed
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+
+    // quit the game on mouse click
+    void OnMouseDown()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/StartGameMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameMenu : MonoBehaviour
+{
+    private string playerName = string.Empty;
+    private byte usernameMaxLength = 14;
+
+    // check is the username valid or not
+    private bool UserName(string username)
+    {
+        bool isValid = true;
+
+        if (username.Length < 1 || username.Length > usernameMaxLength)
+        {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    void OnGUI()
+    {
+        playerName = GUI.TextField(new Rect(140, 240, 147, 24), playerName.ToUpper(), usernameMaxLength);
+        //playerName = GUI.TextField(new Rect(38, 120, 147, 24), playerName.ToUpper(), usernameMaxLength);
+    }
+
+    // ------------------------------------------
+    // change the text color when button pointed
+    void OnMouseEnter()
+    {
+        if (UserName(playerName))
+        {
+            renderer.material.color = Color.green;
+        }
+        //else
+        //{
+        //    renderer.material.color = Color.black;
+        //}
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+
+    // ------------------------------------------
+    // load the game on mouse click
+    void OnMouseDown()
+    {
+        if (UserName(playerName))
+        {
+            Application.LoadLevel("LevelOne");
+        }
+    }
+}
